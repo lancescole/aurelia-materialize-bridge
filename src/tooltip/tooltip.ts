e32e2a4e268eeacce7f06220288c95458b49e0a8
Lance Cole <lancec@saltustech.com>
2019-06-26 08:09:56 -0500

    Added some code to tooltip

--- src/tooltip/tooltip.ts
+++ src/tooltip/tooltip.ts
@@ -12,8 +12,30 @@ export class MdTooltip {
 	delay: number = 50;
 
 	@au.bindable
+	enterDelay: number = 0;
+
+	@au.bindable
+	margin: number = 5;
+
+	@au.bindable
+	inDuration: number = 300;
+
+	@au.bindable
+	outDuration: number = 250;
+
+	@au.bindable
+	transitionMovement: number = 10;
+
+	@au.bindable
 	html: string = "";
 	htmlChanged() {
+		this.initTooltip();
+	}
+
+	@au.bindable
+	text: string = "";
+	textChanged() {
+		this.html = this.text.replace(/<[^>]+>/g, "");
 		this.initTooltip();
 	}
 
@@ -31,7 +53,17 @@ export class MdTooltip {
 
 	initTooltip() {
 		if (this.html) {
-			this.instance = new M.Tooltip(this.element, { exitDelay: this.delay, html: this.html, position: this.position });
+			this.instance = new M.Tooltip(
+				this.element, {
+					exitDelay: this.delay,
+					html: this.html,
+					position: this.position,
+					enterDelay: this.enterDelay,
+					margin: this.margin,
+					inDuration: this.inDuration,
+					outDuration: this.outDuration,
+					transitionMovement: this.transitionMovement
+				});
 		}
 		else if (this.instance) {
 			this.instance.destroy();
